59cc4adc7cedd861637888c8ed33c5e1bc2dbb6a
Xenapsis <149744857+Xenapsis@users.noreply.github.com>
2024-03-05 22:19:26 -0500

    mroe games

--- js/search.js
+++ js/search.js
@@ -12,3 +12,34 @@ for(const gamebox of gamelist.children) {
     gamebox.classList.add('box-closed')
   };
 }});
+
+var searchtag = window.location.search;
+var urlparam = new URLSearchParams(searchtag);
+var tagg = urlparam.get("tag");
+console.log(tagg);
+
+for (const gamebox of gamelist.children) {
+  let tag = gamebox.querySelector('.tags').innerHTML.toLocaleLowerCase();
+  console.log(tag)
+  if (tag.includes(tagg)) {
+    gamebox.removeAttribute('class')
+    gamebox.classList.add('box')
+  } else {
+    gamebox.removeAttribute('class')
+    gamebox.classList.add('box-closed')
+  }
+}
+const searchbytag = document.getElementById('searchTag');
+
+searchbytag.addEventListener("input", (event) => {
+  const input = searchbytag.value.toLowerCase()
+  for(const gamebox of gamelist.children) {
+    let tag = gamebox.querySelector('.tags').innerHTML.toLocaleLowerCase();
+    if (tag.includes(input)) {
+      gamebox.removeAttribute('class')
+      gamebox.classList.add('box')
+    } else {
+      gamebox.removeAttribute('class')
+      gamebox.classList.add('box-closed')
+    };
+  }});
